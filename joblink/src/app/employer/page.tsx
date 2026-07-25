dd91fca02d13c8da57fc7188ecf75aa762ad8883
SHI66ie <ybarkido@gmail.com>
2026-07-25 19:56:28 +0100

    redirect to registration page

--- joblink/src/app/employer/page.tsx
+++ joblink/src/app/employer/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function EmployerPage() {
-  // Redirect to main login page
-  redirect("/");
+  // Redirect to registration page
+  redirect("/signup");
 }
